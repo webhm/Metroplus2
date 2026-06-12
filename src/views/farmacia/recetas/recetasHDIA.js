4a455b1585436bb596bd61ce723a54fd81c8fb95
Web HM <web@hmetro.med.ec>
2026-06-12 18:40:05 -0500

    fix farma 2

--- src/views/farmacia/recetas/recetasHDIA.js
+++ src/views/farmacia/recetas/recetasHDIA.js
@@ -450,7 +450,7 @@ const Recetas = {
                                     m(".d-inline.tx-14", aData.UBICACION + " - " + aData.DS_UNID_INT),
                                     m('br'),
                                     m(".d-inline.tx-semibold.tx-14", aData.TP_STATUS === 'CANCELADO' ? ' USUARIO CANCELA: ' : ' USUARIO: '),
-                                    m(".d-inline.tx-14", aData.TP_STATUS === 'CANCELADO' ? aData.CD_USUARIO + '  a las: ' + aData.FECHA_RECETA : aData.CD_USUARIO),
+                                    m(".d-inline.tx-14", aData.TP_STATUS === 'CANCELADO' ? aData.CD_USUARIO + '  el ' + aData.FECHA_CANCELA : aData.CD_USUARIO),
                                     m('br'),
                                     m(".tx-semibold.tx-14", { "style": (aData.STATUS_DESPACHO == 0 ? "display: none;" : "display: inline;") }, 'DESPACHO: '),
                                     m(".tx-14", { "style": (aData.STATUS_DESPACHO == 0 ? "display: none;" : "display: inline;") }, aData.FECHA_DESPACHO),
